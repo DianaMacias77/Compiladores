a2025c284ad1c6d64f4fa1aff30f5927b766c738
DianaMacias77 <42615630+DianaMacias77@users.noreply.github.com>
2022-10-04 21:19:51 -0500

    Cool inheritsstring.cool, donde Class inherits String (illegal)

--- cool/semantic1/listeners/semantic.py
+++ cool/semantic1/listeners/semantic.py
@@ -1,16 +1,16 @@
-from ..util.exceptions import *
-from ..antlr.coolListener import coolListener
-from ..antlr.coolParser import coolParser
+from util.exceptions import *
+from antlr.coolListener import coolListener
+from antlr.coolParser import coolParser
 
 
 class SemanticListener(coolListener):
     def __init__(self):
         self.main = False
 
-    def enterKlass(self, ctx: coolParser.KlassContext):
-        if ctx.TYPE(0).getText() == 'Main':
-            self.main = True
+    def enterAttribute(self, ctx: coolParser.AttributeContext):
+        if ctx.ID().getText() == 'self':
+            raise BadAttributeName()
 
-    def exitKlass(self, ctx: coolParser.KlassContext):
-        if not self.main:
-            raise nomain()
+    def enterKlass(self, ctx: coolParser.KlassContext):
+        if ctx.TYPE(1) and ctx.TYPE(1).getText() == 'String':
+            raise InvalidInheritance()
